8a6ae8ead03461354aa0b710e8f9a0f758804351
Jon Wynveen <jonwynveen@gmail.com>
2024-05-14 12:33:28 +0300

    project structure

---
+++ src/controllers/api/ingest.controller.js
@@ -0,0 +1,27 @@
+export function post(req, res, next) {
+  // console.log(req.body);
+  let platform = req.params?.platform || req.query?.platform;
+  let userId = req.body?.sender?.id;
+  if (platform && userId) {
+    const io = req.app.locals.io;
+    if (io) {
+      const msg = {
+        user: userId,
+        text: req.body?.message?.text,
+      };
+      let room = `${platform}--${userId}`;
+      console.log('socket send!', room, msg);
+      io.in(room).emit('message', msg);
+    }
+  } else {
+    console.log({
+      platform,
+      userId,
+    });
+  }
+  res.json(req.body);
+};
+
+ export default {
+   post,
+ };
--- src/routes/api/index.js
+++ src/routes/api/index.js
@@ -1,33 +1,10 @@
 import express from 'express';
+import ingestController from '../../controllers/api/ingest.controller.js';
 
 const router = express.Router();
 router.use(express.json()) // for parsing application/json
 
-function ingestHandler (req, res, next) {
-  // console.log(req.body);
-  let platform = req.params?.platform || req.query?.platform;
-  let userId = req.body?.sender?.id;
-  if (platform && userId) {
-    const io = req.app.locals.io;
-    if (io) {
-      const msg = {
-        user: userId,
-        text: req.body?.message?.text,
-      };
-      let room = `${platform}--${userId}`;
-      console.log('socket send!', room, msg);
-      io.in(room).emit('message', msg);
-    }
-  } else {
-    console.log({
-      platform,
-      userId,
-    });
-  }
-  res.json(req.body);
-};
-
-router.post('/ingest', ingestHandler);
-router.post('/ingest/:platform', ingestHandler);
+router.post('/ingest', ingestController.post);
+router.post('/ingest/:platform', ingestController.post);
 
 export default router;
